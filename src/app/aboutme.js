306e2468b8912ce329a5cf054903803de53588d1
vil <123lietuvis5@gmail.com>
2023-09-11 14:09:31 +0100

    minor update

--- src/app/aboutme.js
+++ src/app/aboutme.js
@@ -1,30 +1,55 @@
 'use client'
-import React, { useEffect } from 'react';
+import React, {useEffect,useRef} from 'react';
 import styles from './Aboutme.module.css';
-const AboutmePage = () => {
+
+function AboutmePage() {
+  const revealRef = useRef(null);
+  const skillsRef = useRef(null);
   useEffect(() => {
-    if(typeof window !== "undefined"){
-      const ScrollReveal = require('scrollreveal');
-      const sr = ScrollReveal.default({
-        delay: 100,
-        distance: '500px',
-        origin: 'left',
-        reset: true,
+    if(typeof window !== 'undefined'){
+      import('scrollreveal').then((module) => {
+        const ScrollReveal = module.default;
+        const reveal = {
+          delay: 100,
+          distance: '500px',
+          origin: 'left',
+          reset: true
+        }
+        const skillsReveal = {
+          delay: 1000,
+          distance: '5000px',
+          origin: 'top',
+          reset: true
+        }
+        ScrollReveal().reveal(revealRef.current, reveal);
+        ScrollReveal().reveal(skillsRef.current, skillsReveal);
       });
-      sr.reveal(".reveal");
     }
   }, []);
 
-  return (
-    <div className="reveal">
+  return(
+    <div>
+    <div className="reveal" ref={revealRef}>
       <div className={styles["main"]}>
         <h2>About Me</h2>
         <p className={styles["about-me"]}> 
-            Hello, I am Vilius Radavicius, a dedicated full-stack software engineer fueled by a profound passion for artificial intelligence 
+            Hello, I am Vilius Radavicius, a dedicated full-stack software engineer fueled by a profound passion for artificial intelligence.
         </p>
+        
       </div>
     </div>
-  );
-};
+    <div className="skills" ref={skillsRef}>
+      <div className={styles["main"]}>
+        <h2>Skills</h2>
+        <p className={styles["about-me"]}>
+
+        </p>
+
+      </div>
+    </div>
+    </div>
+    );
+  };
 
 export default AboutmePage;
+
